329b5e6336a19bd09f551d10df0f5d473ebd0bcc
Mike Bishop <mike@miniman-webdesign.co.uk>
2019-01-10 20:28:18 +0000

    Careers page

--- page-careers.php
+++ page-careers.php
@@ -7,35 +7,30 @@
                         <h1 class="vjt_page-title">
                             <?php the_title(); ?> <span><?php _e('VJ Group', 'vjt_theme'); ?></span>
                         </h1>
-                        <h2>Join our team, be a part of the solution.</h2>
+                        <h2><?php the_field('careers_page_subtitle'); ?></h2>
                     </div>
-                    <div class="rte vjt_single-page-content">
-                        For over 30 years, <a href="#">VJ Technologies</a> has been providing our customers with leading-edge solutions for the most advanced X-ray imaging applications. Through our highly specialized divisions and our vast network of global offices, we supply our customers with solutions for any inspection need they may encounter. In order to be successful and remain on the cutting edge we need to hire the best and treat them well. If you are an innovative, energetic individual seeking a new position in a dynamic and exciting environment please consider applying for our current openings:
-                    </div>
-                    <div class="vjt_career-openings">
-                        <h2>current openings:</h2>
-                        <div class="vjt_career-openings-lists">
-                            <div class="vjt_career-openings-list">
-                                <h3>vjt</h3>
-                                <ul>
-                                    <li>Field Service Engineer</li>
-                                </ul>
-                            </div>
-                            <div class="vjt_career-openings-list">
-                                <h3>vjt (NY)</h3>
-                                <ul>
-                                    <li>Sales & Marketing Administrator</li>
-                                </ul>
-                            </div>
-                            <div class="vjt_career-openings-list">
-                                <h3>vjx (NY)</h3>
-                                <ul>
-                                    <li>Test Technician</li>
-                                    <li>Quality Inspector</li>
-                                </ul>
+                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                        <div class="rte vjt_single-page-content">
+                            <?php the_content(); ?>
+                        </div>
+                    <?php endwhile; endif; ?>
+                    <?php if( have_rows('current_openings') ): ?>
+                        <div class="vjt_career-openings">
+                            <h2><?php the_sub_field('current_openings_list_title'); ?></h2>
+                            <div class="vjt_career-openings-lists">
+                                <?php while ( have_rows('current_openings') ) : the_row(); ?>
+                                    <div class="vjt_career-openings-list">
+                                        <h3><?php the_sub_field('current_openings_list_title'); ?></h3>
+                                        <ul>
+                                            <?php if( have_rows('current_openings_list_item') ): while ( have_rows('current_openings_list_item') ) : the_row(); ?>
+                                                <li><?php the_sub_field('current_openings_list_item_role'); ?></li>
+                                            <?php endwhile; endif; ?>
+                                        </ul>
+                                    </div>
+                                <?php endwhile; ?>
                             </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
                 </div>
             </div>
             <div class="vjt_wide-page-wrap-bottom">
@@ -44,48 +39,30 @@
                     <div class="rte vjt_single-page-content">
                         <p>Completed applications can be emailed to: <a href="#">jobopps@vjt.com</a> or faxed to <a href="#">631 589 8992</a></p>
                         <p>We are proud to offer a comprehensive benefit package that will provide our employees with peace of mind knowing they, and their family, are well taken care of.</p>
-                        <h3>Our benefit package includes:</h3>
-                        <ul class="vjt_career-benefits">
-                            <li>Medical Insurance</li>
-                            <li>Dental Insurance</li>
-                            <li>Vision Care Insurance</li>
-                            <li>401(k) Plan with a Generous Company Match</li>
-                            <li>Life Insurance</li>
-                            <li>Short Term Disability Insurance</li>
-                            <li>Long Term Disability Insurance</li>
-                            <li>Employee Assistance Program</li>
-                            <li>Voluntary Supplemental Life Insurance</li>
-                            <li>Voluntary Dependent Life Insurance</li>
-                            <li>Voluntary Supplemental Short Term Disability Coverage</li>
-                            <li>Voluntary Cancer Protection Plan</li>
-                            <li>Voluntary Accident Protection Plan</li>
-                            <li>Long Term Disability Insurance</li>
-                            <li>Employee Assistance Program</li>
-                            <li>Voluntary Supplemental Life Insurance</li>
-                            <li>Voluntary Dependent Life Insurance</li>
-                            <li> Voluntary Supplemental Short Term Disability Coverage</li>
-                            <li>Voluntary Cancer Protection Plan</li>
-                            <li>Voluntary Accident Protection Plan</li>
-                        </ul>
-                        <p>VJ Technologies, and its affiliates, is an equal employment opportunity employer that does not discriminate against individuals based upon their status as protected veterans or individuals with disabilities, and prohibits discrimination against all individuals based upon their race, color, religion, sex, sexual orientation, gender identity or national origin. Moreover, VJT takes affirmative action to employ these individuals and help them to advance in their employment without regard to race, color religion, sex, sexual orientation, gender identity, national origin, disability or veteran status. VJT is an EOE and a VEVRAA Federal Contractor. </p>
+
+                        <?php if( have_rows('benefits_list') ): ?>
+                            <h3>Our benefit package includes:</h3>
+                            <ul class="vjt_career-benefits">
+                                <?php while ( have_rows('benefits_list') ) : the_row(); ?>
+                                    <li><?php the_sub_field('benefits_item'); ?></li>
+                                <?php endwhile; ?>
+                            </ul>
+                        <?php endif; ?>
+
+                        <?php the_field('careers_information_content'); ?>
                         <img src="https://vjt.crucibledigital.co.uk/wp-content/themes/vjt/img/poi-placeholder.png">
-                        <p>Additional information about the Federal and State regulations that guide our recruitment and employment policies can be found via the links below.</p>
-                        <ul>
-                            <li>Federal DOL Notices</li>
-                            <li>Office of Federal Contract Compliance</li>
-                            <li>New York DOL Notices</li>
-                            <li>Connecticut DOL Notices</li>
-                            <li>Massachusetts DOL Notices</li>
-                            <li>Georgia DOL Notices</li>
-                            <li>Idaho DOL Notices</li>
-                            <li>New Mexico DOL Notices</li>
-                            <li>Tennessee DOL Notices</li>
-                            <li>South Carolina DOL Notices</li>
-                            <li>Michigan DOL Notices</li>
-                            <li>California DOL Notices</li>
-                            <li>Arizona DOL Notices</li>
-                            <li>Washington DOL Notices</li>
-                        </ul>
+                        <?php the_field('additional_information_content'); ?>
+                        <?php if( have_rows('additional_information_links') ): ?>
+                            <ul>
+                                <?php while ( have_rows('additional_information_links') ) : the_row(); ?>
+                                    <li>
+                                        <a href="<?php the_sub_field('additional_information_link_target'); ?>" title="<?php the_sub_field('additional_information_link_label'); ?>" target="_blank">
+                                            <?php the_sub_field('additional_information_link_label'); ?>
+                                        </a>
+                                    </li>
+                                <?php endwhile; ?>
+                            </ul>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
