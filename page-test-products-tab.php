2769e84cd74ad49ef7869bd283aa70d700a6cd9f
Mike Bishop <mike@miniman-webdesign.co.uk>
2019-04-15 16:19:56 +0100

    Git update

---
+++ page-test-products-tab.php
@@ -0,0 +1,136 @@
+<?php get_header(); ?>
+<div class="vjt_page-wrap">
+    <div class="vjt_article-archive">
+        <main>
+            <div class="vjt_main-content">
+
+                <h1 class="vjt_page-title">
+                    VJT <span>Products</span>
+                </h1>
+                <nav class="vjt_products-menu vjt_menu">
+                    <ul>
+                        <li><a href="#">Solutions</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="#">Technologies</a></li>
+                        <li><a href="#">Inspection Services</a></li>
+                        <li><a href="#">Resources</a></li>
+                    </ul>
+                </nav>
+                <section class="vjt_product-page-content">
+                    <div class="vjt_product-page-content-copy">
+                    <h1>Vertex II</h1>
+                        <div>
+                            <p>Vertex II offers a simple approach to automated inspection. Programmed sequences and automated provide valuable data for process control, at a fraction of the cost of most automated systems.</p>
+                            <p>Vertex II is configurable with several microfocus x-ray tubes for high resolution applications and high power millimeter focus x-ray sources for castings, munitions, automotive assemblies, aerospace assemblies and other NDT inspections.</p>
+                        </div>
+                    </div>
+                    <div class="vjt_product-page-content-img">
+                        <img src="<?php bloginfo('stylesheet_directory'); ?>/img/vertex.png">
+                    </div>
+                </section>
+                <div class="vjt_product-applications">
+                    <div class="vjt_product-applications-list">
+                        <h2>Applications</h2>
+                        <ul>
+                            <li>Castings</li>
+                            <li>Munitions</li>
+                            <li>Automotive Assemblies</li>
+                            <li>Aerospace Assemblies</li>
+                            <li>Other NDT Inspections</li>
+                        </ul>
+                    </div>
+                    <div class="vjt_product-applications-menu">
+                        <nav class="vjt_menu">
+                            <ul>
+                                <li><a href="#">Solutions</a></li>
+                                <li><a href="#">Products</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="vjt_product-features">
+                    <div class="vjt_product-features-item">
+                        <div class="vjt_product-features-item-img">
+                            <div class="vjt_product-features-item-img-wrap">
+                                <a href="#">
+                                    <img src="<?php bloginfo('stylesheet_directory'); ?>/img/features_placeholder_1.jpg">
+                                    <div class="vjt_image-overlay">Click to View</div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="vjt_product-features-item-content">
+                            <p>Simple-to-use macro tools within Vi3 software suite allows multiple tasks to be completed with one press of a button.</p>
+                        </div>
+                    </div>
+                    <div class="vjt_product-features-item">
+                        <div class="vjt_product-features-item-img">
+                            <div class="vjt_product-features-item-img-wrap">
+                                <a href="#">
+                                    <img src="<?php bloginfo('stylesheet_directory'); ?>/img/features_placeholder_2.jpg">
+                                    <div class="vjt_image-overlay">Click to View</div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="vjt_product-features-item-content">
+                            <p><a href="#">Vi3 Imaging Suite</a> provides easy-to-use powerful imaging filters to highlight the smallest of defects.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <aside>
+            <div class="vjt_main-sidebar">
+                <ul class="vjt_list-dropdown">
+                    <li class="vjt_list-dropdown-header">
+                        <h2>Casting <i class="fas fa-chevron-down"></i></h2>
+                        <ul class="vjt_list-dropdown-child">
+                            <li><a href="#">Vertex II</a></li>
+                            <li><a href="#">C-Arm</a></li>
+                            <li><a href="#">RIX/RIX XL</a></li>
+                            <li><a href="#">T-Conveyor</a></li>
+                            <li><a href="#">In-Line Robotic</a></li>
+                            <li><a href="#">Custom Products</a></li>
+                        </ul>
+                    </li>
+                    <li class="vjt_list-dropdown-header">
+                        <h2>Weld <i class="fas fa-chevron-down"></i></h2>
+                        <ul class="vjt_list-dropdown-child">
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li class="vjt_list-dropdown-header">
+                        <h2>Military <i class="fas fa-chevron-down"></i></h2>
+                        <ul class="vjt_list-dropdown-child">
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li class="vjt_list-dropdown-header">
+                        <h2>Custom Solutions <i class="fas fa-chevron-down"></i></h2>
+                        <ul class="vjt_list-dropdown-child">
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                    <li class="vjt_list-dropdown-header">
+                        <h2>Inspection Services <i class="fas fa-chevron-down"></i></h2>
+                        <ul class="vjt_list-dropdown-child">
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </aside>
+    </div>
+</div>
+<?php get_footer(); ?>
